d694a07724b6e7d52b68369c1ffbbfb2dd9eb33c
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-05-01 02:47:49 +0900

    Update syscall.c

--- xv6/syscall.c
+++ xv6/syscall.c
@@ -103,7 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_uthread(void);
+extern int sys_uthread_init(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,7 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_uthread] sys_uthread,  
+[SYS_uthread] sys_uthread_init,  
 };
 
 void

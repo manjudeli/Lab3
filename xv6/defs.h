0024b8a55505a8ddd46368bc03e9f74db08e1b63
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-05-01 02:47:14 +0900

    Update defs.h

--- xv6/defs.h
+++ xv6/defs.h
@@ -155,6 +155,7 @@ int             argstr(int, char**);
 int             fetchint(uint, int*);
 int             fetchstr(uint, char**);
 void            syscall(void);
+int             uthread_init( );
 
 // timer.c
 void            timerinit(void);

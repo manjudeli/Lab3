844db661528cd717dca77467f237d301b246bd68
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-05-01 13:22:51 +0900

    Update proc.h

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  void (*user_scheduler)(void);
+  uint scheduler;              // address of the user-level scheduler
 };
 
 // Process memory is laid out contiguously, low addresses first:
